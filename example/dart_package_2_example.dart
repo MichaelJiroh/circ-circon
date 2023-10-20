58d96b805faea45cacd8076a5fb02b9686a28f30
MichaelJiroh <mikejiroh@gmail.com>
2023-10-20 01:01:06 -0800

    circle circumference

---
+++ example/dart_package_2_example.dart
@@ -0,0 +1,12 @@
+void main(){
+  print("******************the circumferemce of a circle******************* is");
+  var r;
+  return cir_curmference();
+}
+
+cir_curmference(){
+  var r = 12; //radius
+  const pi = 3.142;
+  var c = 2*pi*r; //circumference
+  print('The circumference of the circle is $c metres');
+}
